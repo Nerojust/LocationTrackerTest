b206bdd616366985c76db8dd658e3ca774c669bf
nerojust <nerojust4@gmail.com>
2019-09-19 11:33:06 +0100

    implemented route tracker

--- app/src/main/java/com/example/locationtrackertest/Activities/MainActivity.java
+++ app/src/main/java/com/example/locationtrackertest/Activities/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.locationtrackertest.Activities;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,6 +20,9 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.room.Room;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.locationtrackertest.Database.LocationDatabase;
 import com.example.locationtrackertest.R;
 import com.google.android.gms.common.ConnectionResult;
@@ -36,6 +41,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,8 +64,7 @@ public class MainActivity extends AppCompatActivity implements
     Button startTracking, stopTracking;
     private GoogleMap mMap;
     //To store longitude and latitude from map
-    private double longitude;
-    private double latitude;
+    private double firstLong, firstLat;
     //From -> the first coordinate from where we need to calculate the distance
     private double currentLongitude;
     private double currentLatitude;
@@ -65,6 +75,8 @@ public class MainActivity extends AppCompatActivity implements
     private GoogleApiClient googleApiClient;
     public static LocationDatabase locationDatabase;
     private com.example.locationtrackertest.Model.Location locationModel;
+    private double firstLatitudeEntry;
+    private double firstLongitudeEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,8 +115,9 @@ public class MainActivity extends AppCompatActivity implements
         startTracking.setOnClickListener(view -> {
             //start the process.
             startTracking.setEnabled(false);
-            stopTracking.setEnabled(true);
+
             getMyLocation();
+            stopTracking.setEnabled(true);
 
         });
         stopTracking.setOnClickListener(view -> {
@@ -113,8 +126,9 @@ public class MainActivity extends AppCompatActivity implements
             stopTracking.setEnabled(false);
             locationManager.removeUpdates(locationListener);
             getLocationFromRoomDB();
-            getFirstLocationInDB();
-            getLastLocationInDb();
+            //zoom in
+            moveMap();
+            getDirection();
         });
     }
 
@@ -151,11 +165,33 @@ public class MainActivity extends AppCompatActivity implements
     private void getLocationFromRoomDB() {
         List<com.example.locationtrackertest.Model.Location> userLocations = locationDatabase.locationDAO().getUserLocationFromDB();
         for (com.example.locationtrackertest.Model.Location loc : userLocations) {
-            int id = loc.getId();
-            double longitude = loc.getLongitude();
-            double latitude = loc.getLatitude();
-            Toast.makeText(this, "user id: " + id + "\n Longitude: " + longitude + "\n Latitude: " + latitude, Toast.LENGTH_SHORT).show();
+            List<com.example.locationtrackertest.Model.Location> locationList = locationDatabase.locationDAO().getUserLocationFromDBbyId(userLocations.get(0).getId());
+            int id = locationList.get(0).getId();
+            firstLongitudeEntry = locationList.get(0).getLongitude();
+            firstLatitudeEntry = locationList.get(0).getLatitude();
+            //Toast.makeText(this, "first entry user id: " + id + "\n Longitude: " + longitude + "\n Latitude: " + latitude, Toast.LENGTH_SHORT).show();
+
+
         }
+//        for (int i = 0; i < userLocations.size(); i++) {
+//
+//            int firstDataId = userLocations.get(0).getId();
+//            com.example.locationtrackertest.Model.Location dd = userLocations.get(userLocations.size() - (userLocations.size()-1));
+//            double firstDataLongitude = dd.getLongitude();
+//            double firstDataLatitude = dd.getLatitude();
+//
+//
+//            Toast.makeText(this, "First user id: " + firstDataId + "\n Longitude: " + firstDataLongitude + "\n Latitude: " + firstDataLatitude, Toast.LENGTH_SHORT).show();
+//        }
+//
+//        for (int i = 0; i < userLocations.size(); i++) {
+//            com.example.locationtrackertest.Model.Location dd = userLocations.get(userLocations.size() - 1);
+//            int lastDataId = dd.getId();
+//            double lastDataLongitude = dd.getLongitude();
+//            double lastDataLatitude = dd.getLatitude();
+//
+//            Toast.makeText(this, "Last user id: " + lastDataId + "\n Longitude: " + lastDataLongitude + "\n Latitude: " + lastDataLatitude, Toast.LENGTH_SHORT).show();
+//        }
     }
 
     private synchronized void setUpGClient() {
@@ -236,10 +272,10 @@ public class MainActivity extends AppCompatActivity implements
                                                 currentLongitude = location.getLongitude();
                                                 currentLatitude = location.getLatitude();
                                                 //save the coords to db
+
                                                 saveTheCoordsToDb();
 
-                                                //zoom in
-                                                moveMap();
+
                                             }
 
                                             @Override
@@ -385,4 +421,111 @@ public class MainActivity extends AppCompatActivity implements
     public void onMarkerDragEnd(Marker marker) {
 
     }
+
+    public String makeURL(double sourcelat, double sourcelog, double destlat, double destlog) {
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("https://maps.googleapis.com/maps/api/directions/json");
+        urlString.append("?origin=");// from
+        urlString.append(Double.toString(sourcelat));
+        urlString.append(",");
+        urlString
+                .append(Double.toString(sourcelog));
+        urlString.append("&destination=");// to
+        urlString
+                .append(Double.toString(destlat));
+        urlString.append(",");
+        urlString.append(Double.toString(destlog));
+        urlString.append("&sensor=false&mode=driving&alternatives=true");
+        urlString.append("&key=AIzaSyBj02AsPhErgBioUFU98Db5AENzi2sgcc4");
+        return urlString.toString();
+    }
+
+    private void getDirection() {
+        //Getting the URL
+        String url = makeURL(firstLatitudeEntry, firstLongitudeEntry, currentLatitude, currentLongitude);
+
+        //Showing a dialog till we get the route
+        final ProgressDialog loading = ProgressDialog.show(this, "Getting Route", "Please wait...", false, false);
+
+        //Creating a string request
+        StringRequest stringRequest = new StringRequest(url,
+                response -> {
+                    loading.dismiss();
+                    //Calling the method drawPath to draw the path
+                    drawPath(response);
+                },
+                error -> loading.dismiss());
+
+        //Adding the request to request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    //The parameter is the server response
+    public void drawPath(String result) {
+        //Getting both the coordinates
+        LatLng from = new LatLng(firstLatitudeEntry, firstLongitudeEntry);
+        LatLng to = new LatLng(currentLatitude, currentLongitude);
+
+//        //Calculating the distance in meters
+//        Double distance = SphericalUtil.computeDistanceBetween(from, to);
+
+        //Displaying the distance
+        //Toast.makeText(this, String.valueOf(distance + " Meters"), Toast.LENGTH_SHORT).show();
+
+
+        try {
+            //Parsing json
+            final JSONObject json = new JSONObject(result);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routes = routeArray.getJSONObject(0);
+            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            String encodedString = overviewPolylines.getString("points");
+            List<LatLng> list = decodePoly(encodedString);
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .addAll(list)
+                    .width(20)
+                    .color(Color.RED)
+                    .geodesic(true)
+            );
+
+
+        } catch (JSONException e) {
+
+        }
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
 }
